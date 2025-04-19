06bf0f06ba122a24cb7597bc9a8f277b231997f5
Azadarx <syedazadarhussayn@gmail.com>
2025-04-19 11:18:35 -0700

    redesigned

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  </StrictMode>
 )
