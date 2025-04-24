7c9d8e1b7f606bef5ddb40eca66b594bf76e092c
Azadarx <syedazadarhussayn@gmail.com>
2025-04-25 01:01:49 +0530

    payment was not happening

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>
-)
+  </React.StrictMode>
+);
